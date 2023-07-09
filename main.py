aba674581c0499245dd1f5499eadc02d5dbb6807
unknown <maxi.lizarraga.91@gmail.com>
2023-07-09 19:12:22 -0300

    finalizada segunda entrega

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from paquete.clase_cliente import Cliente
 from paquete.funciones_entrega_1 import *
 
-cliente1 = Cliente("Maxi","maxi@gmail",30,"Computadoras")
+cliente1 = Cliente("Maxi","maxi@gmail",30)
+cliente2= Cliente("Carlos","carlos@gmail.com",27,"")
 
 print(cliente1)
-print(cliente1.saludar())
+print(cliente1.compra("computacion", "notebook"))
+print(cliente1.compra("computacion","headset"))
+cliente1.historial_de_transacciones()
+print(cliente2.compra("computacion","teclado"))
+print(cliente2.compra("electrodomesticos","heladera"))
+cliente2.historial_de_transacciones()
--- paquete/clase_cliente.py
+++ paquete/clase_cliente.py
@@ -2,16 +2,35 @@
 
 class Cliente:
 
-    def __init__(self,usuario,mail,edad,categorias):
+    def __init__(self,usuario,mail,edad,domicilio):
         self.usuario = usuario
         self.mail = mail
         self.edad = edad
-        self.categorias = categorias
+        self.domicilio = domicilio
+        self.productos = []
+        
 
     def __str__(self):
-        return f"Los datos del cliente son: {self.usuario}, {self.mail}, {self.edad} y {self.categorias}"
+        return f"Los datos del cliente son: \nUsuario:{self.usuario}, \nMail:{self.mail}, \nEdad:{self.edad}, \nDomicilio: {self.domicilio}"
     
-    def saludar(self):
-        return f"Hola {self.usuario}!!"
-        
+    def compra(self,categoria,nombre):
+        self.productos.append(Producto(categoria,nombre))
+        return f"Acabas de comprar: {nombre} de la categoria {categoria}!!"
+    
+    def historial_de_transacciones(self):
+        print(f"El historial de compras de {self.usuario} es: ")
+        for producto in self.productos:
+            print(f"{producto}")
+
+pass
+
+class Producto:
+
+    def __init__(self,categoria, nombre):
+        self.categoria = categoria
+        self.nombre = nombre
+
+    def __str__(self):
+        return f"Categoria:{self.categoria}, Producto:{self.nombre}"
+
 pass
